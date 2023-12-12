87030951e4358cccaa014784baf8f31ea1ede0e7
BUCKERINO <tychovandewetering@gmail.com>
2023-12-12 23:44:11 +0100

    made same stuff (forgot to push last time and cant member what i did)

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -12,9 +12,29 @@ portfolioContainer.addEventListener("click", e =>{
     const modal = modalToggle.parentNode.nextElementSibling
     const closeButton = modal.querySelector('.modal-close')
 
-    modal.classList.add('is-open')
+    const modalOpen = _ => {
+        modal.classList.add('is-open')
+        modal.style.animation = 'modalIn 500ms forwards'
+        document.body.style.overflowY = 'hidden'
+    }
 
-    closeButton.addEventListener("click", _ => {
+    const modalClose = _ =>{
         modal.classList.remove('is-open')
+        modal.removeEventListener('animationend', modalClose)
+    } 
+    
+    closeButton.addEventListener("click", _ => {
+        modal.style.animation = 'modalOut 500ms forwards'
+        modal.addEventListener('animationend', modalClose)
+        document.body.style.overflowY = 'scroll'
     })
+
+    document.addEventListener('keydown', e =>{
+        if(e.keyCOde === 27){
+            modal.style.animation = 'modalOut 500ms forwards'
+            modal.addEventListener('animationend', modalClose)
+        }
+    })
+
+    modalOpen()
 })
